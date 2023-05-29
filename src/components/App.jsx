82a67b0bf8e3f525d040878c45e848de3ef7d13a
Halina Tulchii <halendeihalyna@gmail.com>
2023-05-29 11:27:22 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
+import React, { useEffect, useState } from 'react'
+import TodoList from './TodoList';
+import Context from 'context';
+import Loader from 'Loader';
+
+const AddTodo = React.lazy(() => import('./AddTodo'));
+
+
 export const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+        
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos( todos.map(todo => {
+      if (todo.id === id) {
+      todo.completed = !todo.completed
+      }
+      return todo;
+  }))
+}
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+      {
+        title,
+        id: Date.now(),
+        completed:false,
+      }
+    ]))
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <Context.Provider value={{removeTodo: removeTodo}}>
+    <div className='wrapper'>
+        <h1>React tutorial</h1>
+        <React.Suspense fallback={<p>Loading...</p>}>
+        <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+        
+
+        {loading && <Loader/>}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>You haven't any todo's!</p>
+        )}
+      
     </div>
+    </Context.Provider>
+    
   );
 };
